c8ed5553818bd7f173b7ce3f120ce5be70b4f225
Jacob Kring <jacob.alan.kring@gmail.com>
2021-02-08 16:45:22 -0500

    remove description from equals, not needed

--- assert.go
+++ assert.go
@@ -37,10 +37,10 @@ func NotNil(t *testing.T, err error) {
 }
 
 // Equal fails the test if exp is not equal to act
-func Equal(t *testing.T, description string, exp, act interface{}) {
+func Equal(t *testing.T, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\n\033[31m%s:%d: %s\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n↓\n", filepath.Base(file), line, description, exp, act)
+		fmt.Printf("\n\033[31m%s:%d: \n\texp: %#v\n\n\tgot: %#v\033[39m\n↓\n", filepath.Base(file), line, exp, act)
 		t.FailNow()
 	}
 }
